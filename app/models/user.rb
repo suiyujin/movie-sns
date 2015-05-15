9ee85e317a2721dc5796b19cc85d09214037f9fa
suiyujin <suiyujin04@gmail.com>
2015-05-15 21:16:07 +0900

    log in with twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,30 @@ class User < ActiveRecord::Base
          :omniauthable, omniauth_providers: [:twitter]
 
   validates :name, presence: true
+
+  def self.from_omniauth(auth)
+    # providerとuidでUserレコードを取得する
+    # 存在しない場合は、ブロック内のコードを実行して作成する
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.nickname
+      user.email = auth.info.email
+    end
+  end
+
+  # OmniauthCallbacksControllerでsessionに設定した値をuserオブジェクトにコピー
+  def self.new_with_session(params, session)
+    if session['devise.user_attributes']
+      new(session['devise.user_attributes'], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  # providerがある場合はpasswordは要求しない
+  def password_required?
+    super && provider.blank?
+  end
 end
